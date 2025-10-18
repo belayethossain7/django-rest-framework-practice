74d598f95b8b8665ab22386f35b1e401c15bf4ef
Md. Belayet Hossain <belayet.hossain6847@gmail.com>
2025-10-18 14:49:53 +0600

    practice on api view by using view function api_view, show view decorator

--- apiproject/myapp/views.py
+++ apiproject/myapp/views.py
@@ -6,43 +6,44 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from myapp.models import Contact
 from myapp.serializers import ContactSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-@csrf_exempt
+@api_view(["GET", "POST"])
 def api_list(request):
     
     if request.method == "GET":
-        myapp = Contact.objects.all()
-        serializer = ContactSerializer(myapp, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        contacts = Contact.objects.all()
+        serializer = ContactSerializer(contacts, many=True)
+        return Response(serializer.data)
 
     elif request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = ContactSerializer(data=data)
+        serializer = ContactSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
+
+@api_view(["GET", "PUT", "DELETE"])
 def api_detail(request, pk):
-    
     try:
-        dvar = Contact.objects.get(pk=pk)
+        contact = Contact.objects.get(pk=pk)
     except Contact.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == "GET":
-        serializer = ContactSerializer(dvar)
-        return JsonResponse(serializer.data)
+        serializer = ContactSerializer(contact)
+        return Response(serializer.data)
 
     elif request.method == "PUT":
-        data = JSONParser().parse(request)
-        serializer = ContactSerializer(dvar, data=data)
+        serializer = ContactSerializer(contact, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == "DELETE":
-        dvar.delete()
-        return HttpResponse(status=204)
+        contact.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
